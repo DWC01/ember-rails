828c7094ca161475247313a7804d90ca1e05d9f0
David Christian <david.w.christian@gmail.com>
2014-12-14 12:54:27 -0800

    set emeber-app as rootElement

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,7 +19,7 @@
 //= require ./app
 
 // for more details see: http://emberjs.com/guides/application/
-App = Ember.Application.create();
+App = Ember.Application.create({rootElement: '#ember-app'});
 
 // Log route transitions
 // window.App = Ember.Application.create
