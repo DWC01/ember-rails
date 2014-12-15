827220af93b60bdab8e10f44fd9f5c10e3cd977c
David Christian <david.w.christian@gmail.com>
2014-12-14 16:37:48 -0800

    add search and visitor name filters

--- app/assets/javascripts/controllers/visitors.js
+++ app/assets/javascripts/controllers/visitors.js
@@ -1,3 +1,15 @@
 App.VisitorsController = Ember.ArrayController.extend({
-  sortProperties: ['firstName', 'lastName']
+  sortProperties: ['firstName', 'lastName'],
+
+  visitors: function() {
+    return this.get('search') ? this.get('searchedVisitors') : this
+  }.property('search', 'searchedLeads'),
+
+  searchedVisitors: function() {
+    var search = this.get('search').toLowerCase()
+    return this.filter(function(lead) {
+      return lead.get('fullName').toLowerCase().indexOf(search) != -1
+    })
+  }.property('search', 'this.@each.fullName')
+
 })
